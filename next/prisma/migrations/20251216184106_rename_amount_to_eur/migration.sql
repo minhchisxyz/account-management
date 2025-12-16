401174b105eace51983884fe1362304f44b53b4c
mchisxyz <diepminhchi1617@gmail.com>
2025-12-16 19:55:29 +0100

    migrate database schema

---
+++ next/prisma/migrations/20251216184106_rename_amount_to_eur/migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "Transaction" RENAME COLUMN "amount" TO "amountEUR";
---
+++ next/prisma/migrations/20251216185212_add_column_amount_vnd/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Transaction" ADD COLUMN     "amountVND" DOUBLE PRECISION NOT NULL DEFAULT 0;
