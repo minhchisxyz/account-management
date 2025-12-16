1f9ae39e266a82672abecc0bebc9f8128d272ab3
mchisxyz <diepminhchi1617@gmail.com>
2025-12-16 12:45:37 +0100

    add grouped by date for transactions

--- next-backend/app/api/transactions/years/[year]/months/[month]/route.ts
+++ next-backend/app/api/transactions/years/[year]/months/[month]/route.ts
@@ -1,10 +1,12 @@
-import {NextResponse} from "next/server";
-import {fetchAllTransactionsOfMonth} from "@/app/lib/transaction/data";
+import {NextRequest, NextResponse} from "next/server";
+import {fetchAllTransactionsOfMonth, fetchAllTransactionsOfMonthGroupByDate} from "@/app/lib/transaction/data";
 
 export async function GET(
-    request: Request,
+    request: NextRequest,
     { params }: { params: Promise<{ year: string, month: string }> }
 ) {
   const { year, month } = await params
+  const grouped = request.nextUrl.searchParams.get('grouped')
+  if (grouped) return NextResponse.json(await fetchAllTransactionsOfMonthGroupByDate(Number(year), Number(month)))
   return NextResponse.json(await fetchAllTransactionsOfMonth(Number(year), Number(month)))
 }
